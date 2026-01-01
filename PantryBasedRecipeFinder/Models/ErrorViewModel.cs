1632a8a52f42717e37c7918f78eeec8a509a248a
Omar Moahmed <omvr32@gmail.com>
2026-01-01 15:21:27 -0700

    added json schema for recipes and refactor class names

--- PantryBasedRecipeFinder/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace PantryBasedRecipeFinder.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ RecipeFinder/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RecipeFinder.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
