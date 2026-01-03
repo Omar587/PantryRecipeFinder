870fb4a6cda04cb3cf5f3d337016a8660d3daeae
Omar Moahmed <omvr32@gmail.com>
2026-01-03 11:37:55 -0700

    Added Recipe Controller

---
+++ RecipeFinder/Controllers/RecipeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeFinder.Controllers;
+
+public class RecipeController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public RecipeController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var recipes = _context.Recipes
+            .Include(r => r.Ingredients)
+            .Include(r => r.Tags)
+            .ToList();
+
+        return View(recipes);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var recipe = _context.Recipes
+            .Include(r => r.Ingredients)
+            .Include(r => r.Tags)
+            .Include(r => r.Dietary)
+            .FirstOrDefault(r => r.Id == id);
+
+        if (recipe == null) return NotFound();
+
+        return View(recipe);
+    }
+}
